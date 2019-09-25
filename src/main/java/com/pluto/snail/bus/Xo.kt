3392f566a6f1002ee4628cf7730a27693fcf2e12
pluto <hshsg@live.cn>
2019-09-25 23:42:19 +0800

    增加扩展

---
+++ src/main/java/com/pluto/snail/bus/Xo.kt
@@ -0,0 +1,93 @@
+package com.pluto.snail.bus
+
+import com.alibaba.fastjson.JSON
+
+class XoManage private constructor() {
+    private val CALLBACKS = hashMapOf<Int, IXoListener<*>>()
+
+    fun <T> subscribe(type: Int, listener: IXoListener<T>) {
+        CALLBACKS[type] = listener
+    }
+
+
+    fun <T> execute(type: Int): IXoListener<T> {
+        val block = CALLBACKS[type]
+        if (block != null) {
+            return block as IXoListener<T>
+        }
+        throw NullPointerException("$type IS NULL")
+    }
+
+
+    companion object {
+        val bus = XoManage()
+    }
+}
+
+
+interface IXoListener<T> {
+    fun execute(data: T)
+}
+
+
+object Xo {
+
+
+    fun subsToSrt(type: Int, block: (String) -> Unit): Xo {
+        XoManage.bus.subscribe(type, object : IXoListener<String> {
+            override fun execute(data: String) {
+                block.invoke(data)
+            }
+        })
+        return this
+    }
+
+    fun subsToNum(type: Int, block: (Int) -> Unit): Xo {
+        XoManage.bus.subscribe(type, object : IXoListener<Int> {
+            override fun execute(data: Int) {
+                block.invoke(data)
+            }
+        })
+
+        return this
+    }
+
+    fun subsToNot(type: Int, block: () -> Unit): Xo {
+        XoManage.bus.subscribe(type, object : IXoListener<String> {
+            override fun execute(data: String) {
+                block.invoke()
+            }
+        })
+        return this
+    }
+
+    fun subsTobool(type: Int, block: (Boolean) -> Unit): Xo {
+        XoManage.bus.subscribe(type, object : IXoListener<Boolean> {
+            override fun execute(data: Boolean) {
+                block.invoke(data)
+            }
+        })
+        return this
+    }
+
+    fun busToStr(type: Int, data: String) {
+        XoManage.bus.execute<String>(type).execute(data)
+    }
+
+    fun busToJson(type: Int, data: Any) {
+        val str = JSON.toJSONString(data)
+        XoManage.bus.execute<String>(type).execute(str)
+    }
+
+    fun busToNum(type: Int, data: Int) {
+        XoManage.bus.execute<Int>(type).execute(data)
+    }
+
+    fun busTobool(type: Int, data: Boolean) {
+        XoManage.bus.execute<Boolean>(type).execute(data)
+    }
+
+    fun busToNot(type: Int) {
+        XoManage.bus.execute<String>(type).execute("")
+    }
+}
