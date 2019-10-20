baf0b87e7a9c026854e1847a1e00c7a157dac23c
pluto <hshsg@live.cn>
2019-10-20 21:32:48 +0800

    更新包名

--- src/main/java/com/pluto/snail/SnailApp.kt
+++ src/main/java/com/pluto/snail/SnailApp.kt
@@ -8,12 +8,18 @@ import androidx.multidex.MultiDex
 import androidx.multidex.MultiDexApplication
 import com.pluto.charon.network.interceptors.DebugInterceptor
 import com.pluto.snail.app.Snail
+import me.jessyan.autosize.AutoSizeConfig
+import me.jessyan.autosize.unit.Subunits
 
 open class SnailApp : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         INSTANCE = this
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+        AutoSizeConfig.getInstance().getUnitsManager()
+            .setSupportDP(false)
+            .setSupportSP(false)
+            .setSupportSubunits(Subunits.MM);
     }
 
     override fun attachBaseContext(base: Context?) {
